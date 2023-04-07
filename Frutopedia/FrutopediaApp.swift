580fadd0da7c8af0e28591b18103e0eaaa490e71
Sergey <bindasov.sergey@gmail.com>
2023-04-07 16:21:48 +0200

    init, presets

---
+++ Frutopedia/FrutopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutopediaApp.swift
+//  Frutopedia
+//
+//  Created by Sergey on 07.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FrutopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
